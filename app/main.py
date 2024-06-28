9de45b95e1b46d31a482df492ce191454f85e457
Mark Milburn <mark.a.milburn@gmail.com>
2024-06-28 14:27:33 -0600

    Pass "Return a file"

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 # Uncomment this to pass the first stage
+import argparse
+import os
 import socket
 import threading
 
 echo = "/echo/"
 user_agent = "/user-agent"
+files_path = "/files/"
 
 
 def parse_http_request(request_data):
@@ -34,7 +37,7 @@ def create_http_response(status_code, status_message, content="", content_type="
     return response.encode("utf-8")
 
 
-def handle_client(client_socket, address):
+def handle_client(client_socket, directory_path):
     data = client_socket.recv(4096)
     if data:
         request = parse_http_request(data)
@@ -42,7 +45,7 @@ def handle_client(client_socket, address):
         if path == "/" or path == "" or path is None:
             client_socket.sendall(create_http_response(200, "OK"))
         elif path.startswith(echo):
-            content = path[len(echo):]
+            content = path[len(echo):].strip()
             content_type = "text/plain"
             client_socket.sendall(create_http_response(200, "OK", content, content_type))
         elif path.startswith(user_agent):
@@ -50,6 +53,22 @@ def handle_client(client_socket, address):
             content = request["headers"]["user-agent"]
             content_type = "text/plain"
             client_socket.sendall(create_http_response(200, "OK", content, content_type))
+        elif path.startswith(files_path):
+            filename = path[len(files_path):].strip()
+            file_path = os.path.join(directory_path, filename)
+            if os.path.isfile(file_path):
+                try:
+                    with open(file_path, 'r') as file:
+                        client_socket.sendall(create_http_response(200, "OK", file.read(), "application/octet-stream"))
+                        buffer = file.read()
+                except FileNotFoundError:
+                    client_socket.sendall(create_http_response(404, "Not Found", path))
+                except IOError as e:
+                    client_socket.sendall(create_http_response(500, "Internal Server Error"))
+                except Exception as e:
+                    client_socket.sendall(create_http_response(500, "Internal Server Error"))
+            else:
+                client_socket.sendall(create_http_response(404, "Not Found", path))
         else:
             client_socket.sendall(create_http_response(404, "Not Found", path))
     else:
@@ -58,6 +77,18 @@ def handle_client(client_socket, address):
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Simple webserver')
+    parser.add_argument('--directory', type=str,
+                        help='path to the directory to serve files from')
+
+    args = parser.parse_args()
+
+    # Validate if the directory exists
+    directory_path = args.directory
+    if directory_path is not None:
+        if not os.path.isdir(directory_path):
+            print(f"Error: '{directory_path}' is not a valid directory.")
+            return -1
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
 
@@ -66,7 +97,7 @@ def main():
     server_socket.listen()
     while True:
         client_socket, addr = server_socket.accept()  # wait for client
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, directory_path))
         client_thread.start()
 
 
